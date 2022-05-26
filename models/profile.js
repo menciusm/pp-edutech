35a40c88c179c472a77876d28b2567ce73e6a1c7
galihsatriaadhiputra <galihsadhira@gmail.com>
2022-05-27 00:35:29 +0700

    tambah instance method

--- models/profile.js
+++ models/profile.js
@@ -12,6 +12,9 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       Profile.belongsTo(models.User);
     }
+    helo(){
+      return `Selamat datang ${this.name}` 
+    }
   }
   Profile.init({
     name: {
