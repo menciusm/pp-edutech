c548b9ae78f07781f6946d4e6742e2e88f8ee0b6
menciusm <mencius999@gmail.com>
2022-05-27 07:16:22 +0700

    fix bug

--- routes/admin.js
+++ routes/admin.js
@@ -12,12 +12,4 @@ router.post('/course/:id/edit', Controller.editCourse)
 router.get('/course/:id/enrolled', Controller.courseDetail)
 router.post('/course/:id/enrolled/:userId', Controller.changeStatus)
 
-
-// router.get('/admin/:adminId/courseList', AdmController.adminCourseList)                      //admin list course
-// router.get('/admin/:adminId/courseList/addCourse', AdmController.adminAddCourseForm)         //admin add course form
-// router.post('/admin/:adminId/courseList/addCourse', AdmController.adminAddCourse)            //admin add course
-// router.get('/admin/:adminId/courseList/editCourse', AdmController.adminEditCourseForm)       //admin edit course form
-// router.post('/admin/:adminId/courseList/editCourse', AdmController.adminEditCourseForm)      //admin edit course
-// router.get('/admin/:adminId/courseList/deleteCourse', AdmController.adminDelete)             //admin delete course
-
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -4,29 +4,11 @@ const router = require('express').Router()
 //router index
 router.get('/', Controller.home)
 
-// router.use('/student', require('./routes/student'))
 router.use('/admin', require('./admin'))
-// router.get('/login', LoginController.login)                                                    //login page
-// router.post('/login', LoginController.submit)                                                   //login submit
-// router.post('/logout', LoginController.logout)                                                   //logout submit
+
 
 //====router student
 router.get('/login/:studentsId', Controller.studentPage)                                  //student profile page
-// router.get('/login/:studentsId/courseList', Controller.courseList)                        //student list course
-// router.get('/login/:studentsId/courseList/addCourse', Controller.addCourseForm)           //student add course form
-// router.post('/login/:studentsId/courseList/addCourse', Controller.addCourse)              //student add course
-// router.get('/login/:studentsId/courseList/editCourse', Controller.editCourseForm)         //student edit course form
-// router.post('/login/:studentsId/courseList/editCourse', Controller.editCourseForm)        //student edit course
-// router.get('/login/:studentsId/courseList/deleteCourse', Controller.delete)               //student delete course
-
-//=====router admin
-// router.get('/login/:adminId', AdmController.adminPage)                                       //admin home page
-// router.get('/login/:adminId/courseList', AdmController.adminCourseList)                      //admin list course
-// router.get('/login/:adminId/courseList/addCourse', AdmController.adminAddCourseForm)         //admin add course form
-// router.post('/login/:adminId/courseList/addCourse', AdmController.adminAddCourse)            //admin add course
-// router.get('/login/:adminId/courseList/editCourse', AdmController.adminEditCourseForm)       //admin edit course form
-// router.post('/login/:adminId/courseList/editCourse', AdmController.adminEditCourseForm)      //admin edit course
-// router.get('/login/:adminId/courseList/deleteCourse', AdmController.adminDelete)             //admin delete course
 
 
 module.exports = router
